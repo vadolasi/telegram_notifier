a30f1c25a1ec2ef21d3c55a632485663bbf3e54d
Vitor Daniel <vitor036daniel@gmail.com>
2023-05-18 19:37:06 -0300

    fix initial page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,7 @@ import preact from "@preact/preset-vite"
 import Unocss from "unocss/vite"
 import Pages from "vite-plugin-pages"
 import vercel from "vite-plugin-vercel"
-import ssr from "vite-plugin-ssr/plugin"
 
 export default defineConfig({
-  plugins: [ssr(), vercel(), preact(), Unocss(), Pages({ resolver: "react", importMode: "sync" })]
+  plugins: [vercel(), preact(), Unocss(), Pages({ resolver: "react", importMode: "sync" })]
 })
